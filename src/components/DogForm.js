3a7ec64313337a1e2e3c72fc91771c91b57dd9f2
Student_GA126 <student_ga126@psxga126.lan>
2020-08-10 09:15:31 -0400

    uninstalled bootstrap

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+
 
 const DogForm = () => {
     const [value, setValue] = React.useState({});
@@ -13,14 +13,10 @@ const DogForm = () => {
                 <input type='text'></input>
                 <label>Birthday</label>
                 <input type='text'></input>
-                <label>Gender</label>
-                <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                    <ToggleButton value={1}>Radio 1 (pre-checked)</ToggleButton>
-                    <ToggleButton value={2}>Radio 2</ToggleButton>
-                    <ToggleButton value={3}>Radio 3</ToggleButton>
-                </ToggleButtonGroup>
+                
+
             </form>
-        </div>
+        </div >
     )
 }
 
